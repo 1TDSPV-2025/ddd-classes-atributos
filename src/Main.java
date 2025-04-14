44c8f0b8744414cac1132667fec14b0d93665871
leandroyyy <leandro.cavallari123@gmail.com>
2025-04-14 20:24:50 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Endereco endereco = new Endereco();
+        Cliente cliente = new Cliente();
+        ContaCorrente contaCorrente = new ContaCorrente();
+
+        System.out.println("Digite o logradouro do endereco:");
+        endereco.logradouro = scanner.nextLine();
+
+        System.out.println("Digite o numero do endereco:");
+        endereco.numero = scanner.nextShort();
+        scanner.nextLine();
+
+        System.out.println("Digite o complemento do endereco:");
+        endereco.complemento = scanner.nextLine();
+
+        System.out.println("Digite o cep do endereco:");
+        endereco.cep = scanner.nextLine();
+
+        System.out.println("==============Cliente=================");
+
+        System.out.println("Digite o nome do cliente:");
+        cliente.nome = scanner.nextLine();
+
+        System.out.println("Digite o cpf do cliente:");
+        cliente.cpf = scanner.nextLine();
+
+        cliente.endereco = endereco;
+
+        System.out.println("=============Conta Corrente============");
+
+        System.out.println("Digite um saldo inicial:");
+        contaCorrente.saldo = scanner.nextDouble();
+
+        contaCorrente.titular = cliente;
+
+        System.out.println("Valor a depositar na conta corrente");
+        double saldoADepositar = scanner.nextDouble();
+        contaCorrente.depositar(saldoADepositar);
+        System.out.println("Novo saldo " + contaCorrente.retornarSaldo());
+
+        System.out.println("Valor a retirar da conta corrente");
+        double saldoARetirar = scanner.nextDouble();
+        contaCorrente.retirar(saldoARetirar);
+        System.out.println("Novo saldo " + contaCorrente.retornarSaldo());
+
+        System.out.println("==========Dados do cliente==============");
+        System.out.println(cliente.retornarDados());
+
+        System.out.println("==========Endereco do cliente===========");
+        System.out.println(cliente.endereco.retornarEndereco());
+    }
+}
